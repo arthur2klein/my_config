3c20cf7bd61be1c0fb7214854d2351295b4006cd
arthur2klein <arthur2kleingm@gmail.com>
2024-07-02 22:16:49 +0200

    ADD rnvimr

--- init.lua
+++ init.lua
@@ -34,20 +34,19 @@ require("lazy").setup({
     'junegunn/vim-easy-align',
     'tpope/vim-surround',
     'tpope/vim-commentary',
-    'vim-autoformat/vim-autoformat',
 
     -- File management
     {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' },
     },
     {
       'nvim-treesitter/nvim-treesitter',
       build = ":TSUpdate",
     },
-
+    'kevinhwang91/rnvimr',
     'preservim/tagbar',
-    'preservim/nerdtree',
 
     -- Syntax highlighting
     'udalov/kotlin-vim',
@@ -139,7 +138,6 @@ require("lazy").setup({
           vim.keymap.set("n", "]c", function()
             vim.diagnostic.goto_next({ wrap = false })
           end)
-
         end
 
         return metals_config
@@ -192,7 +190,7 @@ vim.opt.swapfile = false
 
 -- Check for mouse support and enable it
 if vim.fn.has('mouse') == 1 then
-    vim.opt.mouse = 'a'
+  vim.opt.mouse = 'a'
 end
 
 -- Set terminal codes for different modes
@@ -212,11 +210,11 @@ vim.api.nvim_set_keymap('n', '<c-l>', '[s1z=<c-o>', { noremap = true })
 
 -- WSL specific settings
 if vim.fn.has("unix") == 1 then
-    local lines = vim.fn.readfile("/proc/version")
-    if lines[1]:match("Microsoft") then
-        vim.opt.visualbell = true
-        vim.opt.t_u7 = ''
-    end
+  local lines = vim.fn.readfile("/proc/version")
+  if lines[1]:match("Microsoft") then
+    vim.opt.visualbell = true
+    vim.opt.t_u7 = ''
+  end
 end
 
 -- Enable true color support
@@ -229,7 +227,6 @@ vim.cmd('try | colorscheme tokyonight | catch | endtry')
 vim.g.airline_theme = 'tokyonight'
 
 -- Plugin specific settings
-vim.g.NERDTreeQuitOnOpen = 1
 vim.g.airline_powerline_fonts = 1
 vim.g['airline#extensions#tabline#enabled'] = 1
 vim.g.SignatureMarkTextHLDynamic = 1
@@ -238,8 +235,6 @@ vim.g.SignatureMarkTextHLDynamic = 1
 vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
 vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
 vim.api.nvim_set_keymap('n', '<F8>', ':TagbarToggle<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F3>', ':Autoformat<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F2>', ':NERDTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>ml', '<Plug>MarkdownPreview', {})
 vim.api.nvim_set_keymap('n', '<leader>mk', '<Plug>MarkdownPreviewStop', {})
 vim.api.nvim_set_keymap('n', '<silent> <leader>tn', ':TestNearest<CR>', { noremap = true })
@@ -278,34 +273,34 @@ require('mason-lspconfig').setup {
 local lsp = require('lspconfig')
 local coq = require('coq')
 
-lsp.pyright.setup(coq.lsp_ensure_capabilities{})
-lsp.tsserver.setup(coq.lsp_ensure_capabilities{})
-lsp.ansiblels.setup(coq.lsp_ensure_capabilities{})
-lsp.bashls.setup(coq.lsp_ensure_capabilities{})
-lsp.clangd.setup(coq.lsp_ensure_capabilities{})
-lsp.cssls.setup(coq.lsp_ensure_capabilities{})
-lsp.dockerls.setup(coq.lsp_ensure_capabilities{})
-lsp.html.setup(coq.lsp_ensure_capabilities{})
-lsp.jsonls.setup(coq.lsp_ensure_capabilities{})
-lsp.tsserver.setup(coq.lsp_ensure_capabilities{})
-lsp.kotlin_language_server.setup(coq.lsp_ensure_capabilities{})
-lsp.ltex.setup(coq.lsp_ensure_capabilities{})
-lsp.lua_ls.setup(coq.lsp_ensure_capabilities{
+lsp.pyright.setup(coq.lsp_ensure_capabilities {})
+lsp.tsserver.setup(coq.lsp_ensure_capabilities {})
+lsp.ansiblels.setup(coq.lsp_ensure_capabilities {})
+lsp.bashls.setup(coq.lsp_ensure_capabilities {})
+lsp.clangd.setup(coq.lsp_ensure_capabilities {})
+lsp.cssls.setup(coq.lsp_ensure_capabilities {})
+lsp.dockerls.setup(coq.lsp_ensure_capabilities {})
+lsp.html.setup(coq.lsp_ensure_capabilities {})
+lsp.jsonls.setup(coq.lsp_ensure_capabilities {})
+lsp.tsserver.setup(coq.lsp_ensure_capabilities {})
+lsp.kotlin_language_server.setup(coq.lsp_ensure_capabilities {})
+lsp.ltex.setup(coq.lsp_ensure_capabilities {})
+lsp.lua_ls.setup(coq.lsp_ensure_capabilities {
   settings = {
     Lua = {
       diagnostics = {
-        globals = {'vim'},
+        globals = { 'vim' },
       },
     },
   },
 })
-lsp.markdown_oxide.setup(coq.lsp_ensure_capabilities{})
-lsp.intelephense.setup(coq.lsp_ensure_capabilities{})
-lsp.pyright.setup(coq.lsp_ensure_capabilities{})
-lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities{})
-lsp.sqlls.setup(coq.lsp_ensure_capabilities{})
-lsp.terraformls.setup(coq.lsp_ensure_capabilities{})
-lsp.gitlab_ci_ls.setup(coq.lsp_ensure_capabilities{})
+lsp.markdown_oxide.setup(coq.lsp_ensure_capabilities {})
+lsp.intelephense.setup(coq.lsp_ensure_capabilities {})
+lsp.pyright.setup(coq.lsp_ensure_capabilities {})
+lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities {})
+lsp.sqlls.setup(coq.lsp_ensure_capabilities {})
+lsp.terraformls.setup(coq.lsp_ensure_capabilities {})
+lsp.gitlab_ci_ls.setup(coq.lsp_ensure_capabilities {})
 
 -- telescopie
 local builtin = require('telescope.builtin')
@@ -316,6 +311,7 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 
 local actions = require('telescope.actions')
+
 require("telescope").setup({
   defaults = {
     mappings = {
@@ -323,9 +319,23 @@ require("telescope").setup({
         ["<C-k>"] = actions.move_selection_previous,
         ["<C-j>"] = actions.move_selection_next,
       }
-    }
+    },
+    pickers = {
+      git_status = {
+        theme = "dropdown",
+      },
+      find_files = {
+        hidden = true,
+      },
+    },
   }
 })
+vim.api.nvim_set_keymap('n', '<leader>fs', ':Telescope git_status<CR>', { noremap = true, silent = true })
+
+
+vim.g.rnvimr_enable_ex = 1
+vim.g.rnvimr_enable_picker = 1
+vim.api.nvim_set_keymap('n', '<F2>', ':RnvimrToggle<CR>', { noremap = true, silent = true })
 
 -- lsp keymaps
 
@@ -335,11 +345,11 @@ vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
 vim.keymap.set("n", "gds", vim.lsp.buf.document_symbol)
 vim.keymap.set("n", "gws", vim.lsp.buf.workspace_symbol)
-vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run)
-vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help)
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>lc", vim.lsp.codelens.run)
+vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help)
+vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action)
 -- all workspace diagnostics
 vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist)
 -- all workspace errors
