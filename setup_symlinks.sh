3b1f3126d01c2da3703c8e51f96f32151c7095ae
aklein <aklein@anaqua.com>
2025-02-03 11:35:49 +0100

    ADD script to setup symlinks

---
+++ setup_symlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ln -snf ~/my_config/.zshrc ~/.zshrc
+ln -snf ~/my_config/.p10k.zsh ~/.p10k.zsh
+mkdir -p ~/.config/nvim/
+ln -snf ~/my_config/init.lua ~/.config/nvim/init.lua
+ln -snf ~/my_config/.tmux.conf ~/.tmux.conf
+ln -snf ~/my_config/.vimrc ~/.vimrc
+ln -snf ~/my_config/hypr/ ~/.config/hypr
+ln -snf ~/my_config/kitty/ ~/.config/kitty
+ln -snf ~/my_config/rofi/ ~/.config/rofi
+ln -snf ~/my_config/scripts/ ~/.config/scripts
+ln -snf ~/my_config/theme/ ~/.config/theme
+ln -snf ~/my_config/waybar/ ~/.config/waybar
