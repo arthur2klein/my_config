dc3dd2b0281daec5c204f67773e8857418c9cc9e
aklein <aklein@anaqua.com>
2025-04-25 12:55:28 +0200

    feat(select.lua): add default value for select
    If provided and an invalid value is provide to a select, the select will use its
    default value instead of cancelling the action.

--- .zshrc
+++ .zshrc
@@ -149,3 +149,7 @@ export STARSHIP_CONFIG=~/.config/starship.toml
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
--- lua/custom/convcommit/lua/select.lua
+++ lua/custom/convcommit/lua/select.lua
@@ -4,13 +4,14 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local notify = function(message, level)
-	require("notify")(message, level, { title = "Select" })
+  require("notify")(message, level, { title = "Select" })
 end
 
 local M = {}
 
 ---@class SelectOption Props for a select input.
 ---@field prompt string Prompt for the user.
+---@field default string? Default value if no valid selection.
 
 --- Allows the user to input a value amongst a given list of options.
 --- Pressing C-c will cancel the input and the on_choice call.
@@ -18,30 +19,34 @@ local M = {}
 ---@param opts SelectOption Props of the input.
 ---@param on_choice fun(value: string): nil Function that requires the result of the select.
 function M.select(items, opts, on_choice)
-	pickers
-		.new({}, {
-			prompt_title = opts.prompt or "Select an option",
-			layout_config = { height = 15, width = 50 },
-			finder = finders.new_table({
-				results = items,
-			}),
-			sorter = conf.generic_sorter({}),
-			attach_mappings = function(_, map)
-				actions.select_default:replace(function(prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					actions.close(prompt_bufnr)
-					if selection then
-						on_choice(selection[1])
-					else
-						notify("❌ Cancelled.", vim.log.levels.WARN)
-					end
-				end)
-				map("i", "<C-c>", actions.close)
-				map("n", "<C-c>", actions.close)
-				return true
-			end,
-		})
-		:find()
+  pickers
+      .new({}, {
+        prompt_title = opts.prompt or "Select an option",
+        layout_config = { height = 15, width = 50 },
+        finder = finders.new_table({
+          results = items,
+        }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(_, map)
+          actions.select_default:replace(function(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            actions.close(prompt_bufnr)
+            if selection then
+              on_choice(selection[1])
+            else
+              if opts.default ~= nil then
+                on_choice(opts.default)
+              else
+                notify("❌ Cancelled.", vim.log.levels.WARN)
+              end
+            end
+          end)
+          map("i", "<C-c>", actions.close)
+          map("n", "<C-c>", actions.close)
+          return true
+        end,
+      })
+      :find()
 end
 
 return M
