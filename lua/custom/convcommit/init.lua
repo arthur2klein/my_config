ae69d5039f5ead7dafedeec00f4bde2d7b97e463
KLEIN Arthur <arthur2kleingm@gmail.com>
2025-04-26 16:25:34 +0200

    refactor(convcommit): extract convcommit as a separated repository

--- lua/custom/convcommit/init.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.create_version_tag = require("version").create_version_tag
-M.create_commit = require("create_commit").create_commit
-M.git_add = require("git_add").git_add
-M.push = require("git_push").push
-
-return M
