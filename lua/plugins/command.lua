033609a59438686d02df32945783348cc8330f7d
KLEIN Arthur <arthur2kleingm@gmail.com>
2025-06-28 19:08:14 +0200

    feat(nvim): add venn to draw diagrams

--- lua/plugins/command.lua
+++ lua/plugins/command.lua
@@ -164,4 +164,32 @@ return {
 		end,
 	},
 	"mg979/vim-visual-multi",
+	{
+		"jbyuki/venn.nvim",
+		config = function()
+			function _G.Toggle_venn()
+				local venn_enabled = vim.inspect(vim.b.venn_enabled)
+				if venn_enabled == "nil" then
+					vim.b.venn_enabled = true
+					vim.cmd([[setlocal ve=all]])
+					vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
+					vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
+					vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
+					vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+					vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+					vim.api.nvim_buf_set_keymap(0, "v", "m", "jlokh", { noremap = true })
+				else
+					vim.cmd([[setlocal ve=]])
+					vim.api.nvim_buf_del_keymap(0, "n", "J")
+					vim.api.nvim_buf_del_keymap(0, "n", "K")
+					vim.api.nvim_buf_del_keymap(0, "n", "L")
+					vim.api.nvim_buf_del_keymap(0, "n", "H")
+					vim.api.nvim_buf_del_keymap(0, "v", "f")
+					vim.api.nvim_buf_del_keymap(0, "v", "m")
+					vim.b.venn_enabled = nil
+				end
+			end
+			vim.api.nvim_set_keymap("n", "<leader>v", ":lua Toggle_venn()<CR>", { noremap = true })
+		end,
+	},
 }
