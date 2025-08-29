314dd8c8d103422015ba5fbacdd237d03bc78b18
aklein <aklein@anaqua.com>
2025-08-29 09:28:14 +0200

    fix(nvim): Increase formating timeout

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,40 +1,45 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function()
-			local conform = require("conform")
-			conform.setup({
-				formatters = {
-					markdownlint = {
-						command = "markdownlint",
-						stdin = true,
-					},
-				},
-				formatters_by_ft = {
-					lua = { "stylua" },
-					python = { "black" },
-					c = { "clang_format" },
-					cl = { "clang_format" },
-					glsl = { "clang_format" },
-					php = { "pretty-php" },
-					rust = { "rustfmt" },
-					sql = { "sql_formatter" },
-					tex = { "latexindent" },
-					markdown = { "doctoc" },
-					javascript = { "prettier", stop_after_first = true },
-					typescript = { "prettier", stop_after_first = true },
-					typescriptreact = { "prettier", stop_after_first = true },
-					javascriptreact = { "prettier", stop_after_first = true },
-				},
-			})
+  {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+      local conform = require("conform")
+      conform.setup({
+        default_format_opts = {
+          async = false,
+          timeout_ms = 2500,
+          lsp_fallback = true,
+        },
+        formatters = {
+          markdownlint = {
+            command = "markdownlint",
+            stdin = true,
+          },
+        },
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "black" },
+          c = { "clang_format" },
+          cl = { "clang_format" },
+          glsl = { "clang_format" },
+          php = { "pretty-php" },
+          rust = { "rustfmt" },
+          sql = { "sql_formatter" },
+          tex = { "latexindent" },
+          markdown = { "doctoc" },
+          javascript = { "prettier", stop_after_first = true },
+          typescript = { "prettier", stop_after_first = true },
+          typescriptreact = { "prettier", stop_after_first = true },
+          javascriptreact = { "prettier", stop_after_first = true },
+        },
+      })
 
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function()
-					conform.format({ async = false, lsp_fallback = true })
-				end,
-			})
-		end,
-	},
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function()
+          conform.format({ async = false, lsp_fallback = true })
+        end,
+      })
+    end,
+  },
 }
