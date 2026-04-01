8005d19c70c195aae34166620b5c8bcd4b241215
aklein <arthur2kleingm@gmail.com>
2026-04-01 13:54:09 +0200

    fix(weather): more precise icon

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
-wmo_to_icon() {
+wmo_translation() {
   case "$1" in
-    0) echo "󰖙" ;;  # clear sky
-    1|2) echo "󰖕" ;;  # partly cloudy
-    3) echo "󰖐" ;;  # overcast
-    45|48) echo "󰖑" ;;  # fog
-    51|53|55) echo "󰖗" ;;  # drizzle
-    61|63|65) echo "󰖖" ;;  # rain
-    71|73|75) echo "󰼶" ;;  # snow
-    80|81|82) echo "󰖗" ;;  # showers
-    95|96|99) echo "󰖓" ;;  # thunderstorm
-    *) echo "󰖐" ;;  # default
+    0) echo "󰖙";; # Clear sky
+    1) echo "";; # Mainly clear
+    2) echo "";; # Partly cloudy
+    3) echo "";; # Overcast
+    45) echo "󰖑";; # Fog
+    48)	echo "";; # Depositing rime fog
+    51) echo "󰸊";; # Light drizzle
+    53) echo "󰖌";; # Moderate drizzle
+    55) echo "";; # Dense drizzle
+    56) echo "󰸊";; # Light freezing drizzle
+    57) echo "";; # Dense freezing drizzle
+    61) echo "󰖗";; # Slight rain
+    63) echo "";; # Moderate rain
+    65)	echo "";; # Heavy rain
+    66) echo "󰖗";; # Light freezing rain
+    67)	echo "";; # Heavy freezing rain
+    71) echo "󰖘";; # Slight snow fall
+    73) echo "󰼶";; # Moderate snow fall
+    75) echo "";; # Heavy snow fall
+    77)	echo "";; # Snow grains
+    80) echo "";; # Slight rain showers
+    81) echo "";; # Moderate rain showers
+    82) echo "";; # Violent rain showers
+    85) echo "";; # Slight snow showers
+    86) echo "";; # Heavy snow showers
+    95)	echo "";; # Thunderstorm
+    96) echo "";; # Thunderstorm with slight hail
+    99) echo "";; # Thunderstorm with heavy hail
+    *) echo "" ;; # Unknown
   esac
 }
 
-data=$(curl https://api.open-meteo.com/v1/forecast\?latitude\=43.3112\&timezone=Europe%2FParis\&longitude\=-0.3558\&current\=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,weather_code)
+data=$(curl https://api.open-meteo.com/v1/forecast\?latitude\=43.3112\&timezone=Europe%2FParis\&longitude\=-0.3558\&current\=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation_probability,weather_code)
 current_temperature="$(echo "$data" | jq -r '.current.temperature_2m')°C"
-current_humidity="$(echo "$data" | jq -r '.current.relative_humidity_2m')%"
+current_humidity="$(echo "$data" | jq -r '.current.relative_humidity_2m')"
 current_wind="$(echo "$data" | jq -r '.current.wind_speed_10m')km/h"
-current_precipitation="$(echo "$data" | jq -r '.current.precipitation')mm"
-current_weather_code=$(wmo_to_icon $(echo "$data" | jq -r '.current.weather_code'))
+current_weather_code=$(wmo_translation $(echo "$data" | jq -r '.current.weather_code'))
 
-echo "$current_weather_code $current_temperature $current_humidity $current_precipitation $current_wind"
+echo "$current_weather_code $current_temperature $current_humidity $current_wind"
