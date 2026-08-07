2f230c3d5f951be47b2051a29228f1165e6c7d86
aklein <arthur2kleingm@gmail.com>
2026-08-07 18:08:41 +0200

    fix(hypr): adapt to latest version

--- scripts/quick_terminal.sh
+++ scripts/quick_terminal.sh
@@ -4,7 +4,7 @@ CLASS="scratchpad"
 WS="magic_scratchpad"
 
 if hyprctl clients | grep -q "class: $CLASS"; then
-  hyprctl dispatch togglespecialworkspace "$WS"
+  hyprctl dispatch "hl.dsp.workspace.toggle_special('$WS')"
 else
   $1 --class "$CLASS"
 fi
